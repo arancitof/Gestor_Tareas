b0b21d4b1bf2ca15831e48e61576508e019b44b0
Alan Flores <alanpfqa@gmail.com>
2025-07-22 14:34:45 -0600

    build

--- src/App.js
+++ src/App.js
@@ -22,8 +22,6 @@ import Navbar from './components/pure/Navbar';
 function App() {
   return (
     <div className="App">
-      {/*       <header className="App-header"> */}
-      {/*         <img src={logo} className="App-logo" alt="logo" /> */}
       {/* Componente Gretting */}
       {/*         <Gretting name='Alan'></Gretting> */}
       {/* <GrettingF name='Alan'></GrettingF> */}
